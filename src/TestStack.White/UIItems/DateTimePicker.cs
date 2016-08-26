66ef51ee7523938f6f644771f9ca030d49dc6ef9
danxi <sys88515170@gmail.com>
2016-08-26 15:54:12 +0100

    fix issue that Datepicker with min & Max limit sometimes cannot correct get date set. Use up and Down keyword to set date

--- src/TestStack.White/UIItems/DateTimePicker.cs
+++ src/TestStack.White/UIItems/DateTimePicker.cs
@@ -1,4 +1,4 @@
-using System;
+﻿using System;
 using System.Windows.Automation;
 using TestStack.White.Configuration;
 using TestStack.White.UIItems.Actions;
@@ -34,12 +34,36 @@ namespace TestStack.White.UIItems
                 return;
             }
 
-            keyboard.Send(dateFormat.DisplayValue(dateTime.Value, 0).ToString(), actionListener);
-            keyboard.PressSpecialKey(KeyboardInput.SpecialKeys.RIGHT, actionListener);
-            keyboard.Send(dateFormat.DisplayValue(dateTime.Value, 1).ToString(), actionListener);
-            keyboard.PressSpecialKey(KeyboardInput.SpecialKeys.RIGHT, actionListener);
-            keyboard.Send(dateFormat.DisplayValue(dateTime.Value, 2).ToString(), actionListener);
-            keyboard.PressSpecialKey(KeyboardInput.SpecialKeys.RIGHT, actionListener);
+            /*
+            * Yishun:  26/08/2016 - For Winform Datepicker with min & Max limit, sometimes numbers will not be
+            * truely entered, as the number out of it's range.  
+            * Here we use the Up (↑）and down(↓) keys to move year, month and days.
+            */
+            if (Date != null)
+            {
+                KeyboardInput.SpecialKeys upOrDownKey;
+                int diff = 0;
+                
+                for (int datePart = 2; datePart >= 0; datePart--)
+                {
+                    //move left to highligh year first(default Day is highlighted)
+                    keyboard.PressSpecialKey(KeyboardInput.SpecialKeys.LEFT, actionListener);
+                    diff = dateFormat.DisplayValue(dateTime.Value, datePart) - dateFormat.DisplayValue(Date.Value, datePart);
+                    upOrDownKey = diff > 0 ? KeyboardInput.SpecialKeys.UP : KeyboardInput.SpecialKeys.DOWN;
+                    for (int i = 0; i < Math.Abs(diff); i++) keyboard.PressSpecialKey(upOrDownKey, actionListener);
+                }
+                
+            }
+            else
+            {
+                keyboard.Send(dateFormat.DisplayValue(dateTime.Value, 0).ToString(), actionListener);
+                keyboard.PressSpecialKey(KeyboardInput.SpecialKeys.RIGHT, actionListener);
+                keyboard.Send(dateFormat.DisplayValue(dateTime.Value, 1).ToString(), actionListener);
+                keyboard.PressSpecialKey(KeyboardInput.SpecialKeys.RIGHT, actionListener);
+                keyboard.Send(dateFormat.DisplayValue(dateTime.Value, 2).ToString(), actionListener);
+                keyboard.PressSpecialKey(KeyboardInput.SpecialKeys.RIGHT, actionListener);
+            }
+
         }
     }
 }
